99948b925fe566669dca9cf4c51710a1baff453d
Guxtavo. <88405978+guxtavodev@users.noreply.github.com>
2024-10-16 09:40:05 -0300

    Update avaliar_professores.tsx

--- pages/avaliar_professores.tsx
+++ pages/avaliar_professores.tsx
@@ -23,6 +23,17 @@ const AvaliarProfessores = () => {
     const [professorIndex, setProfessorIndex] = useState(0);
     const [sugestao, setSugestao] = useState('');
 
+    const perguntas = [
+        "Demonstra conhecimento do conteúdo trabalhado",
+        "Demonstra ter preparado bem a aula",
+        "Explica a matéria com clareza despertando interesse e participação?",
+        "Acompanha os trabalhos, é atencioso e esclarece adequadamente as dúvidas?",
+        "Demonstra domínio da turma?",
+        "Estimula o aluno a participar das aulas?",
+        "Mantém a disciplina e a ordem na sala?",
+        "Utiliza adequadamente o tempo da aula?"
+    ];
+
     useEffect(() => {
         if (turmaId) {
             const fetchProfessores = async () => {
@@ -78,9 +89,9 @@ const AvaliarProfessores = () => {
             <Navbar />
             <h1 className="text-2xl font-bold mb-4 text-center">Avaliação do Professor: {professor.nome}</h1>
             <form onSubmit={handleSubmit} className="space-y-6">
-                {[...Array(14)].map((_, index) => (
+                {perguntas.map((pergunta, index) => (
                     <div key={index} className="flex flex-col space-y-2">
-                        <Label htmlFor={`avaliacao_${index + 1}`}>{index + 1}. Pergunta {index + 1}:</Label>
+                        <Label htmlFor={`avaliacao_${index + 1}`}>{index + 1}. {pergunta}</Label>
                         <Select name={`avaliacao_${index + 1}`} onValueChange={(value) => handleChange({ target: { name: `avaliacao_${index + 1}`, value } })} required>
                             <SelectTrigger className="w-full">
                                 <SelectValue placeholder="Selecione uma opção" />
@@ -88,10 +99,10 @@ const AvaliarProfessores = () => {
                             <SelectContent>
                                 <SelectGroup>
                                     <SelectLabel>Opções</SelectLabel>
-                                    <SelectItem value="A">Ótimo</SelectItem>
-                                    <SelectItem value="B">Bom</SelectItem>
-                                    <SelectItem value="C">Regular</SelectItem>
-                                    <SelectItem value="D">Necessita Melhorar</SelectItem>
+                                    <SelectItem value="Otimo">Ótimo</SelectItem>
+                                    <SelectItem value="Bom">Bom</SelectItem>
+                                    <SelectItem value="Regular">Regular</SelectItem>
+                                    <SelectItem value="Necessita melhorar">Necessita Melhorar</SelectItem>
                                 </SelectGroup>
                             </SelectContent>
                         </Select>
